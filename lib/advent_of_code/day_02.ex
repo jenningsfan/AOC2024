4ac03ee020213a72ae4a76f37565903f4f629669
Sammy Humphreys <Sammyh2009@outlook.com>
2024-12-07 21:11:22 +0000

    Solve day 2 part 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -14,17 +14,36 @@ defmodule AdventOfCode.Day02 do
     end
   end
 
+  # all differnces same sign and between 1 and 3
+  def is_report_safe(report) do
+    Enum.all?(report, fn x -> abs(x) >= 1 && abs(x) <= 3 end) &&
+      (Enum.all?(report, fn x -> x > 0 end) or Enum.all?(report, fn x -> x < 0 end))
+  end
+
+  def dampen(report) do
+      for i <- 0..length(report) do
+        List.delete_at(report, i)
+      end
+  end
+
   def solve_part1(input) do
-    input = Enum.map(input, fn x -> x
+    Enum.map(input, fn x -> x
       |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.map(fn [x1, x2] -> x1 - x2 end)
+      |> Enum.map(fn [x1, x2] -> x1 - x2 end) end)
+    |> Enum.count(fn x -> is_report_safe(x) end)
+  end
+
+  def solve_part2(input) do
+    Enum.map(input, fn r ->
+      for d <- dampen(r) do
+        d
+        |> Enum.chunk_every(2, 1, :discard) # split into chunks
+        |> Enum.map(fn [x1, x2] -> x1 - x2 end) # find the differences between the each value
+        |> is_report_safe
+      end
+      |> Enum.count(fn x -> x end) # count how many times it has succeded with the damping
     end)
-
-    differ_ok = Enum.map(input, fn row -> Enum.all?(row, fn x -> abs(x) >= 1 && abs(x) <= 3 end) end)
-    sign_ok = Enum.map(input, fn row -> Enum.all?(row, fn x -> x > 0 end) or Enum.all?(row, fn x -> x < 0 end) end)
-
-    Enum.zip(differ_ok, sign_ok)
-    |> Enum.count(fn {d, s} -> d && s end)
+    |> Enum.count(fn x -> x != 0 end) # any that succeded at least once
   end
 
   def part1(args) do
@@ -33,6 +52,9 @@ defmodule AdventOfCode.Day02 do
     |> solve_part1
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_input
+    |> solve_part2
   end
 end
